7818c8eeec7ae489f21fe2692c6a063f658bcb41
Jane Claire <pixiephreak@Janes-MacBook-Pro.local>
2017-03-26 10:21:12 -0400

    work on snapshot

--- assets/app.js
+++ assets/app.js
@@ -9,7 +9,8 @@
 // Initialize Firebase
 
 function App(){
-
+	// console.log(this);
+	this.self = this;
 	this.database = firebase.database();
 	this.start = document.getElementById('start');
 
@@ -26,14 +27,24 @@ function App(){
 		losses:0
 	}
 
-	// database.ref().on("child_added", snapshot(snapshot));
-
-	// function snapshot(snapshot) {
-	// 	//check if player one exists yet. if player1, set player2 status. if player1 !exist, set player1 status
-	// 	if (snapshot.child('player1').exists()){
-	// 		document.getElementById('player1-status').innerHTML()
-	// 	}
-	// }
+	this.database.ref().on('child_added', function(snapshot) {
+		console.log(this);
+		console.log('in fb');
+			// TO-DO : how to nav to children?
+			console.log(snapshot.val().name);
+		//check if player one exists yet in FB
+		if (snapshot.child('player1').exists()){
+			console.log('in fb');
+			console.log(snapshot.val().player1);
+			// set value in local object to vaule from FB
+			this.player1 = snapshot.val().player1;
+			// update html to refelct current val in fb
+		}else{
+			// document.getElementById('update').innerHTML = `${playerName}, you are player 2`
+			// player2.name = playerName;
+			// document.getElementById('form').style.visibility = "hidden";
+		}
+	});
 
 	this.start.addEventListener("click", this.displayPlayer.bind(this));
 
@@ -47,22 +58,17 @@ App.prototype.displayPlayer = function(event) {
 		document.getElementById('update').innerHTML = `${playerName}, you are player 1`
 		player1.name = playerName;
 		document.getElementById("player1").innerHTML = `<p>${player1.name}</p><span>Wins:${this.wins} Losses:${this.losses}</span>`;
-		  // Save the new price in Firebase
-		this.database.ref().set({
+		  // Save the new price in Firebase. TO-DO use set or push?
+		this.database.ref().push({
 		  player1 : player1
 		});
 		document.getElementById('form').style.visibility = "hidden";
 
 	}else{
-		document.getElementById('update').innerHTML = `${playerName}, you are player 2`
-		player2.name = playerName;
-		document.getElementById("player2").innerHTML = `<p>${player2.name}</p><span>Wins:${this.wins} Losses:${this.losses}</span>`;
-		this.database.ref().set({
-		  player2 : player2
-		});
-		document.getElementById('form').style.visibility = "hidden";
+
 	}
 }
+
 
 window.onload = function() {
   window.App = new App();
